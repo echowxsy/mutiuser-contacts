81ecbd92811771df9bec4f98740bb675b7af2456
Echowxsy <i@echowxsy.com>
2019-04-26 02:26:12 +0800

    docs: 添加apidoc注释

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -8,6 +8,30 @@ let validator = require('validator');
 class UserController {
   constructor() {}
 
+  /**
+   * @api {POST} /v1/regist 用户注册
+   * @apiGroup User
+   * @apiParam  {String} email 邮箱
+   * @apiParam  {String} password 密码
+   * @apiSuccessExample 200
+   *     {
+   *       "code": 0,
+   *       "message": "注册成功",
+   *       "data": {}
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 100,
+   *       "message": "邮箱已经注册",
+   *       "data": {},
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 1,
+   *       "message": "错误的参数",
+   *       "data": {},
+   *     }
+   */
   async regist(ctx) {
     const {
       email,
@@ -33,6 +57,33 @@ class UserController {
     ctx.returnValue(ResConstant.REGIST_SUCCESS.key);
   }
 
+  /**
+   * @api {POST} /v1/login 用户登陆
+   * @apiGroup User
+   * @apiParam  {String} email 邮箱
+   * @apiParam  {String} password 密码
+   * @apiSuccessExample 200
+   *     {
+   *       "code": 0,
+   *       "message": "登陆成功",
+   *       "data": {
+   *          "token": "eyJhbGci.asdaisdasdasd.dasdasdasdasdas",
+   *          "refresh_token": "easdasd.sasdasdas.asdasdasda"
+   *        }
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 102,
+   *       "message": "密码错误",
+   *       "data": {},
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 1,
+   *       "message": "错误的参数",
+   *       "data": {},
+   *     }
+   */
   async login(ctx) {
     const {
       email,
@@ -70,6 +121,31 @@ class UserController {
     })
   }
 
+  /**
+   * @api {PUT} /v1/user 用户修改密码
+   * @apiGroup User
+   * @apiHeader {String} Authorization token
+   * @apiParam  {String} oldpasswd 旧密码
+   * @apiParam  {String} password 密码
+   * @apiSuccessExample 200
+   *     {
+   *       "code": 0,
+   *       "message": "密码修改成功",
+   *       "data": {}
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 102,
+   *       "message": "密码错误",
+   *       "data": {},
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 1,
+   *       "message": "错误的参数",
+   *       "data": {},
+   *     }
+   */
   async changePassword(ctx) {
     const {
       oldpassword,
@@ -95,6 +171,31 @@ class UserController {
     ctx.returnValue(ResConstant.UPDATE_PASSWORD_SUCCESS.key)
   }
 
+ /**
+   * @api {PUT} /v1/oauth/refreshtoken 刷新token
+   * @apiGroup User
+   * @apiHeader {String} Authorization refresh_token
+   * @apiParam  {String} oldpasswd 旧密码
+   * @apiParam  {String} password 密码
+   * @apiSuccessExample 200
+   *     {
+   *       "code": 0,
+   *       "message": "Token刷新成功",
+   *       "data": {token:'asdasdasd'}
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 105,
+   *       "message": "refresh_token未到使用时间",
+   *       "data": {},
+   *     }
+   * @apiErrorExample 200
+   *     {
+   *       "code": 1,
+   *       "message": "错误的参数",
+   *       "data": {},
+   *     }
+   */
   async refreshToken(ctx) {
     const userId = ctx.passport.userId;
     const email = ctx.passport.email;
